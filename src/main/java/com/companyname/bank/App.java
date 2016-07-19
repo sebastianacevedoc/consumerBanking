133c7601c3a0cc33209121cef45f9a84f8a5577e
Sebastián Acevedo C <sebastian.acevedoc@usach.cl>
2016-07-19 03:57:09 -0300

    test

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,10 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "First commit from maven ^^" );
-=======
         System.out.println( "Hello World!" );
->>>>>>> 449b47512ed2950429d0986f63d582f3c80f715f
     }
 }
